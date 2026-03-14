fe040a7a5bd606c5b1e4e5d14b8a70f2a465781c
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-03-14 14:52:58 +0100

    Update test_dht11.ino

--- src/test_dht11.ino
+++ src/test_dht11.ino
@@ -1,1 +1,35 @@
+#include <DHT.h>
 
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Sensor read failed");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("--------------------");
+
+  delay(2000);
+}
