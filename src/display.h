85fcdbc5538c84f31be489e2ddf192549d2094c8
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-03-14 13:41:56 +0100

    Create initial environment sensor project scaffold

---
+++ src/display.h
@@ -0,0 +1,7 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void initDisplay();
+void showReading(float value);
+
+#endif  // DISPLAY_H
---
+++ src/sensor.h
@@ -0,0 +1,7 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+void initSensor();
+float readSensorValue();
+
+#endif  // SENSOR_H
